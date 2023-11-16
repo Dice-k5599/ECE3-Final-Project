2b8aaebc5a887e7eed488d17e275c3956e53090b
Dice-k5599 <108194726+Dice-k5599@users.noreply.github.com>
2023-11-16 12:02:33 -0800

    Update ECE3_Car.ino

--- ECE3_Car.ino
+++ ECE3_Car.ino
@@ -1,24 +1,53 @@
 #include <ECE3.h>
 
+// array for storing sensor value of previous tick and current tick
 uint16_t sensorValues[8];
+uint16_t prev[8];
+
+// left wheel pin definitions
 const int left_nslp_pin=31; // nslp ==> awake & ready for PWM
 const int left_dir_pin=29;
 const int left_pwm_pin=40;
 
+// right wheel pin definitions
 const int right_nslp_pin=11; // nslp ==> awake & ready for PWM
 const int right_dir_pin=30;
 const int right_pwm_pin=39;
 
+// LED pin definition
 const int LED_RF = 41;
+
+// variables storing wheel speed (in pwm); value range: (0,255)
 int left_spd;
 int right_spd;
-const float Kp = 0.1;
 
-const float mins[8] = {483,506,628.6,510.8,437,460,460,599};
-const float maxs[8] = {1993,1736.8,1871.4,1329,1159,1159,903.8,1901};
-const float weights[8] = {-19.0, -17.0, -13.0, -9.0, 8.0, 12.0, 14.0, 15.0};
-uint16_t prev[8];
+int next_left_spd;
+int next_right_spd;
 
+// variable related to turn amount
+// - turn amount (turn_amt) = Kp * error + Kd * dError (change in error)
+float error = 0;
+float prevError = 0;
+float deltaError = error - prevError;
+int turn_amt = 0;
+int max_turn_amt = 0;
+
+
+// constants storing values needed to calculate turn amount
+const float mins[8] = {483.0,506,628.6,510.8,437.0,460.0,460.0,599.0};
+const float maxs[8] = {1993.0,1736.8,1871.4,1329.0,1159.0,1098.0,903.8,1901.0};
+const float weights[8] = {-15.0, -14.0, -12.0, -8.0, 8.0, 12.0, 14.0, 15.0};
+
+
+// Base speed, Kp and Kd values (subject to change over iterations)
+
+//Kp should be a value so that max error (around +/- 20.15) yeilds BASE_SPD / 2;
+//
+const float BASE_SPD = 30.0;
+const float Kp = (BASE_SPD * 0.5) / 20.15;
+const float Kd = 0.0;
+
+// bools that store 
 bool started;
 bool uturn;
 
@@ -53,13 +82,27 @@ void setup()
   delay(50);
 }
 
+
+// each weighting value calculated by equation
+// weight * ((raw_value - min_value) * 10) / max_value
+// sum all and divide by 8 to get turnAmount
+
 float turnAmount(uint16_t x[]) {
-  
   float sum = 0;
   for (int i = 0; i < 8; i++) {
-    sum = sum + (weights[i] * (x[i] - mins[i]) / maxs[i]);
+    sum += (weights[i] * ((x[i] - mins[i]) * 10.0) / maxs[i]);
   }
-  return sum;
+  return sum / 8.0;
+}
+
+bool darkLineDetected(uint16_t x[]) {
+  float threshold = 1600;
+  for (int i = 0; i < 8; i++)
+  {
+    if (x[i] < threshold)
+      return false;
+  }
+  return true;
 }
 
 
@@ -71,22 +114,14 @@ void loop()
   //sensorValues[i]
 
   //check to see if the car is reading a black line across all sensors (start or finish line)
-  if (sensorValues[0] > 1600 && sensorValues[1] > 1600 &&
-  sensorValues[2] > 1600 && sensorValues[3] > 1600 &&
-  sensorValues[4] > 1600 && sensorValues[5] > 1600 &&
-  sensorValues[6] > 1600 && sensorValues[7] > 1600 &&
-  prev[0] > 1600 && prev[1] > 1600 &&
-  prev[2] > 1600 && prev[3] > 1600 &&
-  prev[4] > 1600 && prev[5] > 1600 &&
-  prev[6] > 1600 && prev[7] > 1600) {
+  if (darkLineDetected(prev) && darkLineDetected(sensorValues)) {
     if (!started) {
       digitalWrite(LED_RF,HIGH);
       delay(50);
       digitalWrite(LED_RF,LOW);
+      left_spd = BASE_SPD;
+      right_spd = BASE_SPD;
       started = true;
-      left_spd = 30;
-      right_spd = 30;
-      
     }
     else {
       uturn == true;
@@ -99,31 +134,41 @@ void loop()
 
   //use the amount below to change the speed of the wheels
 
-  float error = turnAmount(sensorValues);
-  int turn_amt = Kp * error;
-  if (started) {
-    Serial.println(turn_amt);
-      if (left_spd + (turn_amt) >= 15 && left_spd + (turn_amt) < 45) {
-        left_spd = left_spd - (Kp * error);
-      }
-      if (right_spd - (turn_amt) < 45 && right_spd - (turn_amt) >= 15) {
-        right_spd = right_spd + (turn_amt);
-      }
-    
-    
+  // calculates error & dError and turn amount using equation:
+  //  - turn_amt = -Kp * error + -Kd * dError
+  error = turnAmount(sensorValues);
+  deltaError = error - prevError;
+  turn_amt = (-1.0) * Kp * error + (-1.0) * Kd * deltaError;
 
+  // set prevError as current error for next loop
+  prevError = error;
+
+  // calculate next speed for each wheel for boundary check
+  next_left_spd = left_spd + turn_amt;
+  next_right_spd = right_spd - turn_amt;
+  
+  if (started) {
+    if (next_left_spd >= (BASE_SPD - BASE_SPD * 0.5) && next_left_spd < (BASE_SPD + BASE_SPD * 0.5)) {
+      left_spd = next_left_spd;
+    }
+    if (next_right_spd >= (BASE_SPD - BASE_SPD * 0.5) && next_right_spd < (BASE_SPD + BASE_SPD * 0.5)) {
+      right_spd = next_right_spd;
+    }
   }
 
-  //Serial.print(right_spd);
-  //Serial.print(" ");
-  //Serial.println(left_spd);
+//  Serial.print("Turn amount      : ");
+//  Serial.println(turn_amt);
+//  Serial.print("Right wheel speed: ");
+//  Serial.println(right_spd); 
+//  Serial.print("Left wheel speed : ");
+//  Serial.println(left_spd);
+//  Serial.println();
   analogWrite(left_pwm_pin,left_spd);
   analogWrite(right_pwm_pin,right_spd);
 
   for (int i = 0; i < 8; i++) {
-    prev[i] = sensorValues[i];
-    
+    prev[i] = sensorValues[i];  
   }
-  delay(50);
+  //delay(1000);
 
 }
